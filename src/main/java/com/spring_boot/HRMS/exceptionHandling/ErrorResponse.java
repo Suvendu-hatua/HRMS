3a9d75b574c77e5e1983bb373a3ddaa77ebe736c
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-24 11:12:26 +0530

    Adding ErrorResponse pojo in exceptionHandling class

---
+++ src/main/java/com/spring_boot/HRMS/exceptionHandling/ErrorResponse.java
@@ -0,0 +1,41 @@
+package com.spring_boot.HRMS.exceptionHandling;
+
+public class ErrorResponse {
+    private String message;
+    private int statusCode;
+    private long timeStamp;
+
+    //constructor
+
+    public ErrorResponse(String message, int statusCode, long timeStamp) {
+        this.message = message;
+        this.statusCode = statusCode;
+        this.timeStamp = timeStamp;
+    }
+
+    //setter and getter
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+}
