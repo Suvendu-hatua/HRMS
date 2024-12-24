73cf25b69e661229e8a84bea31055cd838029dbf
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-24 10:56:09 +0530

    Implementing custom UserDetailService for authentication

---
+++ src/main/java/com/spring_boot/HRMS/config/HRMSUserDetailsService.java
@@ -0,0 +1,47 @@
+package com.spring_boot.HRMS.config;
+
+import com.spring_boot.HRMS.dao.HrDao;
+import com.spring_boot.HRMS.entity.HR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class HRMSUserDetailsService implements UserDetailsService {
+
+    private HrDao hrDao;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public HRMSUserDetailsService(HrDao hrDao,PasswordEncoder passwordEncoder) {
+        this.hrDao = hrDao;
+        this.passwordEncoder=passwordEncoder;
+    }
+
+    /**
+     * @param email
+     * @return UserDetails
+     * @throws UsernameNotFoundException
+     */
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        HR hr=hrDao.findByEmail(email).orElseThrow(()->new UsernameNotFoundException("can't find Hr profile with email:"+email));
+
+        System.out.println(hr);
+
+        //Getting list of authorities.
+        Collection<GrantedAuthority> authorities= List.of(new SimpleGrantedAuthority(hr.getRole()));
+
+        return new User(hr.getEmail(),hr.getPassword(),authorities);
+    }
+}
