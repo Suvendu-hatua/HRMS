22f7136c54938d82d5d79e6ac2fc942b7e9e3c5b
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-24 15:37:08 +0530

    Adding Admin entity and implementing one-to-one with person

---
+++ src/main/java/com/spring_boot/HRMS/entity/Admin.java
@@ -0,0 +1,82 @@
+package com.spring_boot.HRMS.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "admin")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "mobile_no",length = 10)
+    private String mobileNumber;
+
+    @OneToOne
+    @JoinColumn(name = "person_id",nullable = false)
+    private Person person;
+
+    //constructor
+
+    public Admin() {
+    }
+    //setter and getter
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+//toString()
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", mobileNumber='" + mobileNumber + '\'' +
+                '}';
+    }
+}
