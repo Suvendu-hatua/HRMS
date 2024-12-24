250f8a1558789ec9f396a79210071fda548d37d1
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-24 10:54:35 +0530

    Adding and Implementing HRService class

---
+++ src/main/java/com/spring_boot/HRMS/service/HRService.java
@@ -0,0 +1,76 @@
+package com.spring_boot.HRMS.service;
+
+import com.spring_boot.HRMS.dao.HrDao;
+import com.spring_boot.HRMS.entity.HR;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class HRService {
+
+    private HrDao hrDao;
+
+    @Autowired
+    public HRService(HrDao hrDao) {
+        this.hrDao = hrDao;
+    }
+
+    /**
+     * Find Hr by id
+     * @param id
+     * @return HR
+     */
+
+    public HR getHrById(long id){
+       return hrDao.findById(id).orElseThrow(()->new RuntimeException("Hr profile is not found with id:"+id));
+    }
+
+    /**
+     * Save Hr profile
+     * @param hr
+     * @return
+     */
+    @Transactional
+    public HR saveHrProfile(HR hr){
+        return hrDao.save(hr);
+    }
+
+    /**
+     * Update Hr profile
+     * @param id
+     * @param updatedHr
+     * @return
+     */
+
+    @Transactional
+    public HR updateHrProfile(long id,HR updatedHr){
+        HR hr=getHrById(id);
+        if(hr!=null){
+            hr.setEmail(updatedHr.getEmail());
+            hr.setFirstName(updatedHr.getFirstName());
+            hr.setLastName(updatedHr.getLastName());
+            hr.setPassword(updatedHr.getPassword());
+            hr.setRole(updatedHr.getRole());
+            return hrDao.save(hr);
+        }
+        throw new RuntimeException("can't update hr profile.");
+    }
+
+    /**
+     * Delete Hr profile
+     * @param id
+     */
+
+    @Transactional
+    public void deleteHrProfileById(long id){
+        HR hr=getHrById(id);
+        try{
+            hrDao.delete(hr);
+        }
+        catch (Exception e){
+            throw new RuntimeException("can't find HR profile to delete by id:"+id);
+        }
+    }
+
+}
