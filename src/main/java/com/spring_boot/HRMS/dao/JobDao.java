cdd9c508b62d6e49bbc19e85b59a54da5e447335
Suevndu Hatua <suvenduhatua01@gmail.com>
2024-12-29 19:47:10 +0530

    Adding @Query annotation in JobDao class to enhance search operation by job title and location

--- src/main/java/com/spring_boot/HRMS/dao/JobDao.java
+++ src/main/java/com/spring_boot/HRMS/dao/JobDao.java
@@ -2,10 +2,15 @@ package com.spring_boot.HRMS.dao;
 
 import com.spring_boot.HRMS.entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface JobDao extends JpaRepository<Job,String> {
-        List<Job> findByTitle(String title);
-        List<Job> findByLocation(String location);
+        @Query("SELECT j FROM Job j WHERE LOWER(j.title) LIKE LOWER(CONCAT('%', :title, '%'))")
+        List<Job> findByTitle(@Param("title") String title);
+
+        @Query("SELECT  j FROM Job j WHERE LOWER(j.location) like LOWER(CONCAT('%', :location, '%'))")
+        List<Job> findByLocation(@Param("location") String location);
 }
