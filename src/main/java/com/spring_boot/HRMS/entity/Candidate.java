5b521e4eb5fed3855c01efd38361f8accd100036
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-01-03 18:59:58 +0530

    Adding @ToString annotation

--- src/main/java/com/spring_boot/HRMS/entity/Candidate.java
+++ src/main/java/com/spring_boot/HRMS/entity/Candidate.java
@@ -1,10 +1,7 @@
 package com.spring_boot.HRMS.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
@@ -14,6 +11,7 @@ import java.util.List;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Candidate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
