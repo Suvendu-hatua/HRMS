8864ab0ef1b180a7ab5eca3122fb610bdca0e4c0
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-01-05 00:45:59 +0530

    Implementing DTO and mapping structure in AdminService class and according to that modifying code.

--- src/main/java/com/spring_boot/HRMS/service/AdminService.java
+++ src/main/java/com/spring_boot/HRMS/service/AdminService.java
@@ -1,38 +1,56 @@
 package com.spring_boot.HRMS.service;
 
 import com.spring_boot.HRMS.dao.AdminDao;
+import com.spring_boot.HRMS.dtos.AdminDTO;
+import com.spring_boot.HRMS.dtos.AdminPostDTO;
 import com.spring_boot.HRMS.entity.Admin;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.spring_boot.HRMS.exceptionHandling.ProfileNotFoundException;
+import com.spring_boot.HRMS.mapper.AdminMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class AdminService {
 
-    private AdminDao adminDao;
+    private final AdminDao adminDao;
+    private final PasswordEncoder passwordEncoder;
+    private final AdminMapper adminMapper;
 
-    /**
-     * @param adminDao
-     */
-    @Autowired
-    public AdminService(AdminDao adminDao) {
-        this.adminDao = adminDao;
+    public AdminDTO getAdminById(long id){
+        Admin admin= adminDao.findById(id).orElseThrow(()->new RuntimeException("Admin profile is not found with id:"+id));
+        //converting Admin to AdminDTO
+        return adminMapper.toDTO(admin);
     }
 
-    public Admin getAdminById(long id){
-        return adminDao.findById(id).orElseThrow(()->new RuntimeException("Admin profile is not found with id:"+id));
+    @Transactional
+    public Admin addAdmin(AdminPostDTO adminPostDTO){
+        Admin admin=adminMapper.toEntity(adminPostDTO);
+        //setting admin role
+        admin.getPerson().setRole("ROLE_ADMIN");
+        //encrypting password
+        admin.getPerson().setPassword(passwordEncoder.encode(admin.getPerson().getPassword()));
+        //saving into DB
+        return adminDao.save(admin);
     }
-
     /**
      * @param id
-     * @param updatedAdmin
+     * @param adminPostDTO
      * @return
      */
     @Transactional
-    public Admin updateAdminProfile(long id, Admin updatedAdmin){
+    public Admin updateAdminProfile(long id, AdminPostDTO adminPostDTO){
         if(adminDao.existsById(id)){
-            updatedAdmin.setId(id);
-            return adminDao.save(updatedAdmin);
+            Admin admin=adminDao.findById(id).orElseThrow(()->new ProfileNotFoundException("Can;t find Admin profile by id:"+id));
+
+            //updating fields.
+            admin.setFirstName(adminPostDTO.getFirstName());
+            admin.setLastName(adminPostDTO.getLastName());
+            admin.setMobileNumber(adminPostDTO.getMobileNumber());
+
+            return adminDao.save(admin);
         }
         throw new RuntimeException("can't update Admin profile.");
     }
