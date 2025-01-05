24f1e33c7dab803c6ba898074416beea4c5ebc15
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-01-05 23:43:57 +0530

    Adding findByPersonEmail() method in HrDao to find HR based on email id

--- src/main/java/com/spring_boot/HRMS/dao/HrDao.java
+++ src/main/java/com/spring_boot/HRMS/dao/HrDao.java
@@ -2,7 +2,12 @@ package com.spring_boot.HRMS.dao;
 
 import com.spring_boot.HRMS.entity.HR;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface HrDao extends JpaRepository<HR,Long> {
-
+    @Query("SELECT h FROM HR h WHERE h.person.email = :email")
+        Optional<HR> findByPersonEmail(@Param("email") String email);
 }
