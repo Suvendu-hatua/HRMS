9b07005e83a04fe6bb210ed9d33896a6c078dd57
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-01-13 00:46:11 +0530

    Adding and implementing CustomAccessDeniedHandler for handling Authorization failed access

---
+++ src/main/java/com/spring_boot/HRMS/exceptionHandling/CustomAccessDeniedHandler.java
@@ -0,0 +1,38 @@
+package com.spring_boot.HRMS.exceptionHandling;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import java.io.IOException;
+
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+    /**
+     * @param request               that resulted in an <code>AccessDeniedException</code>
+     * @param response              so that the user agent can be advised of the failure
+     * @param accessDeniedException that caused the invocation
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        String message = (accessDeniedException != null && accessDeniedException.getMessage() != null) ? accessDeniedException.getMessage()
+                : "Authorization Failed";
+        String path = request.getRequestURI();
+        //Setting headers and content type
+        response.setHeader("hrms-denied-reason","Authorization Failed");
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType("application/json;charset=UTF-8");
+
+        //Construct JSON Response
+        String jsonResponse=String.format("{\"timeStamp\":\"%s\",\"status\":\"%d\",\"error\":\"%s\",\"message\":\"%s\",\"path\":\"%s\"}",
+                System.currentTimeMillis(),HttpStatus.FORBIDDEN.value(),HttpStatus.FORBIDDEN.getReasonPhrase(),
+                message,path
+        );
+        response.getWriter().write(jsonResponse);
+    }
+}
