a9fed97a4e20b80cef63ce3bbf62a518a3ca0f2b
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-01-19 18:33:41 +0530

    Updating SecurityConfiguration file to enabling CSRF  protection and session management

--- src/main/java/com/spring_boot/HRMS/config/SecurityConfiguration.java
+++ src/main/java/com/spring_boot/HRMS/config/SecurityConfiguration.java
@@ -2,13 +2,18 @@ package com.spring_boot.HRMS.config;
 
 import com.spring_boot.HRMS.exceptionHandling.CustomAccessDeniedHandler;
 import com.spring_boot.HRMS.exceptionHandling.CustomBasicAuthenticationEntryPoint;
+import com.spring_boot.HRMS.filter.CsrfTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 public class SecurityConfiguration {
@@ -29,8 +34,12 @@ public class SecurityConfiguration {
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrfCon->csrfCon.disable()).
-                authorizeHttpRequests(requests-> requests
+        CsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler=new CsrfTokenRequestAttributeHandler();
+
+        http.securityContext(securityContext->securityContext.requireExplicitSave(false))
+                //Session Management for Spring Security
+                .sessionManagement(sessionConfig->sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                . authorizeHttpRequests(requests-> requests
                         .requestMatchers("/hr/**").hasRole("HR")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/candidate/**").hasRole("CANDIDATE")
@@ -42,6 +51,15 @@ public class SecurityConfiguration {
                         ).permitAll()
                         .anyRequest().authenticated()
                 );
+
+        //CSRF Protection
+        http.csrf(csrfConfig->csrfConfig
+                .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler)
+                .ignoringRequestMatchers(
+                        "/register")
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+        //CSRF/XSRF token will be generated after Basic Authentication is complete.
+        http.addFilterAfter(new CsrfTokenFilter(), BasicAuthenticationFilter.class);
 
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(hbc->hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
