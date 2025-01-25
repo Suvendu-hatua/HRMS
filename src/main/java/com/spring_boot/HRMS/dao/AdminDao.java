907b12014e4c3d6051d3beb4acab9ca3162f7d5f
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-01-25 15:31:22 +0530

    Adding findByPersonEmail() method in AdminDao for retrieving admin details using userEmail

--- src/main/java/com/spring_boot/HRMS/dao/AdminDao.java
+++ src/main/java/com/spring_boot/HRMS/dao/AdminDao.java
@@ -2,7 +2,12 @@ package com.spring_boot.HRMS.dao;
 
 import com.spring_boot.HRMS.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface AdminDao extends JpaRepository<Admin,Long> {
-
+    @Query("SELECT a FROM Admin a WHERE a.person.email = :email")
+    Optional<Admin> findByPersonEmail(@Param("email") String email);
 }
