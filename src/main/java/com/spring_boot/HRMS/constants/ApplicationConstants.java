e9bea8f0441261afed1f9d96e09a3c8c7f7165c4
Suevndu Hatua <suvenduhatua01@gmail.com>
2025-01-25 15:30:15 +0530

    Adding ApplicationConstants class for JWT_TOKEN related keys and default values

---
+++ src/main/java/com/spring_boot/HRMS/constants/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.spring_boot.HRMS.constants;
+
+public final class ApplicationConstants {
+
+    public static String JWT_SECRET_KEY="JWT_SECRET";
+    public static String JWT_SECRET_DEFAULT_VALUE="jxgEQeXHuPq8VdbyYFNkANdudQ53YUn4";
+    public static String JWT_HEADER="Authorization";
+}
